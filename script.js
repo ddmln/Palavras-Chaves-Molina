3e51e87b001283b23935abdeacf1d0198f88b69f
ddmln <molina.eduarda@escola.pr.gov.br>
2025-06-17 12:26:27 +0000

    Aula 3, apliquei o campo resultadoo

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-
 const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
 
 botaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
 
 function mostraPalavrasChave() {
     const text = document.querySelector('#entrada-de-texto').value;
-    
+    const campoResultado = document.querySelector ('#resultado-palavrachave'
+
+        campoResultado.textContent = texto;
 }
     

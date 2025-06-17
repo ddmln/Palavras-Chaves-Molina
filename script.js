d5dc4b3d9a60660bc73d6bf15fb9d7dff2e9f536
ddmln <molina.eduarda@escola.pr.gov.br>
2025-06-17 12:21:29 +0000

    Aula 3, corrigi umas coisas

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
+
 const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
 
 botaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
 
 function mostraPalavrasChave() {
-
+    const text = document.querySelector('#entrada-de-texto').value;
+    
 }
+    

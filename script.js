d2a2dba1e4fa00ffbef60184cdcb50c26ab548eb
ddmln <molina.eduarda@escola.pr.gov.br>
2025-06-29 18:07:49 -0300

    Aula 6, ordenamos mais o processaTexto

--- script.js
+++ script.js
@@ -12,12 +12,26 @@ function mostraPalavrasChave() {
 
 function processaTexto(texto) {
     let palavras = texto.split(/\P{L}+/u);
+    const frequencias = contaFrequencias(palavras);
+    let ordenadas = Object.keys(frequencias).sort(ordenaPalavra);
 
-    let frequencias = {};
-    for (let i of palavras) {
-      
+    function ordenaPalavra(p1, p2) {
+        return frequencias[p2] - frequencias[p1];
     }
 
+    console.log(ordenadas);
+    return ordenadas.slice(0, 10);
+}
 
-    return palavras;
+function contaFrequencias(palavras) {
+    let frequencias = {};
+    for (let i of palavras) {
+        frequencias[i] = 0;
+        for (let j of palavras) {
+            if (i == j) {
+                frequencias[i]++;
+            }
+        }
+    }
+    return frequencias;
 }
